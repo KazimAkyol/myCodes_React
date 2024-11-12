c032a1f620250789a5ed0416f1e5b1c97a74dc59
KazimAkyol <akyolkzm.2016493@gmail.com>
2024-11-12 22:56:12 +0100

    10.commit

--- react/RC-05-06-Events-Hooks/src/1-events-hooksintro/Events.jsx
+++ react/RC-05-06-Events-Hooks/src/1-events-hooksintro/Events.jsx
@@ -1,22 +1,61 @@
-import React from 'react'
+import React from "react";
 
 const Events = () => {
-    //! JavaScript alani
+  //! JavaScript alani
+  let baslik = "MERHABA";
+  let sayac = 0;
 
+  const arttir = () => {
+    sayac = sayac + 1;
+
+    // console.log(sayac);
+
+    document.querySelector("span").textContent = sayac;
+  };
+
+  const changeBaslik = (title) => {
+    baslik = title;
+
+    // console.log(baslik);
+
+    document.querySelector("h1").textContent = title;
+  };
+
+  const click = () => {
+    document.querySelector(".btn-danger").textContent = "TIKLANDI";
+  };
 
   return (
     //! jsx(react) alani
 
-    <div className='container text-center mt-4'>
-        <h1>INFO:</h1>
+    <div className="container text-center mt-4">
+      <h1>INFO:{baslik}</h1>
 
-        <h2>COUNT:<span className='text-danger'></span></h2>
+      <h2>
+        COUNT:<span className="text-danger">{sayac}</span>
+      </h2>
 
-        <button className='btn btn-primary'></button>
-        <button className='btn btn-info'></button>
-        <button className='btn btn-danger'></button>
+      <button onClick={arttir} className="btn btn-primary">
+        ARTTIR
+      </button>
+      <button onClick={() => changeBaslik("HALLO")} className="btn btn-info">
+        BASLIK DEGISTIR
+      </button>
+      <button onClick={click} className="btn btn-danger">
+        TIKLA
+      </button>
     </div>
-  )
-}
+  );
+};
 
-export default Events
+export default Events;
+
+//?-------------------------------------------------------------------
+//! Konsolda, güncellenen değişen count'u görebiliriz ancak web sayfasında (biz görüntüle demeden) görüntülenmiyor!!!
+//* Çünkü, REACT herhangi bir elementi default olarak static sayar.
+//* DOM manipülasyonunu en aza indirmek için bunu yapar.
+//* Hangi elementin interactive olduğu konusunda React'e bilgi vermeliyiz.
+//! REACT'i interactive elements hakkında bilgilendirmek için iki yöntem kullanıyoruz:
+//* 1. Statefull Classes. State-based inform (Class Components)
+//* 2. Hooks (Functional Components)
+//?--------------------------------------------------------------------
