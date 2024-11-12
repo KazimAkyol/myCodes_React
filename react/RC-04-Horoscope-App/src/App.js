ab4ed5d529eb4fac8b87fb31a7995f1bb56d8593
KazimAkyol <akyolkzm.2016493@gmail.com>
2024-11-12 14:02:56 +0100

    seventh commit

--- react/RC-04-Horoscope-App/src/App.js
+++ react/RC-04-Horoscope-App/src/App.js
@@ -2,6 +2,7 @@ import React from "react";
 import "./App.scss";
 import Navbar from "./components/Navbar/Navbar";
 import Header from "./components/Header/Header";
+import Main from "./components/Main/Main";
 
 const App = () => {
   return (
@@ -9,6 +10,8 @@ const App = () => {
       <Navbar />
 
       <Header />
+
+      <Main />
     </div>
   );
 };
