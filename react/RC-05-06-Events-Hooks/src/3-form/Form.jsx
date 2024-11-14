1c3857a2d3f2c29dedefa4a614b1658139615e1e
KazimAkyol <akyolkzm.2016493@gmail.com>
2024-11-14 22:26:15 +0100

    15.commit

--- react/RC-05-06-Events-Hooks/src/3-form/Form.jsx
+++ react/RC-05-06-Events-Hooks/src/3-form/Form.jsx
@@ -1,4 +1,4 @@
-import React from 'react'
+import React, { useState } from "react";
 
 //! JSX ile HTML arasında çok fazla fark olmamakla birlikte bazı önemli farklar da mevcuttur.
 //! HTML'de class olarak tanımlanan nitelik JSX'de className olarak tanımlanmalı.
@@ -9,9 +9,49 @@ import React from 'react'
 //! Ayrıca JSX'de JavaScript ifadeleri {} içinde çalıştırılabilir. Buna map, && gibi yapılar da dahil.
 
 const Form = () => {
-  return (
-    <div>Form</div>
-  )
-}
+  const [isim, setIsim] = useState("");
+  const [pass, setPass] = useState("");
+  const [country, setCountry] = useState("");
 
-export default Form
+  return (
+    <div className="container text-center mt-4">
+      <h1>*********************************************</h1>
+
+      <h1>FORM(EVENTS)</h1>
+
+      <form>
+        <div className="mb-3">
+          <label className="form-label" htmlFor="isim">
+            NAME: <span className="text-danger"> </span>
+          </label>
+          <input className="form-control" id="isim" type="text" 
+          onInput={(e)=>setIsim(e.target.value)}
+           />
+        </div>
+        <div className="mb-3">
+          <label className="form-label" htmlFor="password">
+            PASSWORD
+          </label>
+          <input className="form-control" id="password" type="password" />
+        </div>
+
+        <div className="mb-3">
+          <label className="form-label" htmlFor="country">
+            COUNTRY: <span className="text-danger"> </span>
+          </label>
+          <select className="form-select">
+            <option selected>COUNTRIES</option>
+            <option value="Türkiye">TURKEY</option>
+            <option value="Almanya">GERMANY</option>
+            <option value="Amerika">USA</option>
+          </select>
+        </div>
+        <button type="submit" className="btn btn-primary">
+          GÖNDER (submit)
+        </button>
+      </form>
+    </div>
+  );
+};
+
+export default Form;
