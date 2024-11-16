740f146f74b98d24f551768310d5392cf08ae953
KazimAkyol <akyolkzm.2016493@gmail.com>
2024-11-16 22:26:53 +0100

    22.commit

--- react/RC-01-ReactIntro/src/msg/Msg.jsx
+++ react/RC-01-ReactIntro/src/msg/Msg.jsx
@@ -1,18 +1,30 @@
+import "./Msg.css";
 
-import "./Msg.css"
+import resim from "../assets/tree.webp";
+import resim2 from "../assets/winter1.jpg";
 
 const Msg = () => {
   return (
     <div>
-        <h2>MSG ALANI</h2>
+      <h2>MSG ALANI</h2>
 
-        <p>hosgeldin react</p>
+      <p>hosgeldin react</p>
 
-        <img src="" alt="" />
-        <img src="" alt="" />
-        <img src="" alt="" />
+      {/* public'in icindeki img */}
+      <img src="./assets/winter1.jpg" alt="" />
+
+      {/* src icincdeki img */}
+      <img src={resim} alt="" />
+      <img src={resim2} alt="" />
+
+      {/* google dan aldık */}
+      <img
+        width="300px"
+        src="https://cdn.pixabay.com/photo/2023/06/13/16/25/baby-8061294_1280.jpg"
+        alt=""
+      />
     </div>
-  )
-}
+  );
+};
 
-export default Msg
+export default Msg;
