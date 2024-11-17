7f251e0e8f0327cf098149af12bd75238ad17f76
KazimAkyol <akyolkzm.2016493@gmail.com>
2024-11-17 11:34:27 +0100

    25.commit

--- react/RC-02-Props/src/App.js
+++ react/RC-02-Props/src/App.js
@@ -1,7 +1,25 @@
 import "./App.css";
+import Person from "./Person";
 
 function App() {
-  return <div>App</div>;
+  const people = [
+    {
+      name: "Osman Kara",
+      img: "https://cdn.pixabay.com/photo/2017/08/01/01/33/beanie-2562646__480.jpg",
+      tel: "555 55 55",
+    },
+    {
+      name: "Ayşe Güler",
+      img: "https://cdn.pixabay.com/photo/2020/10/19/09/44/woman-5667299_1280.jpg",
+      tel: "555 55 55",
+    },
+  ];
+  return (
+    <div>
+      <Person kisi={people[0]} />
+      {/* <Person kisi={people[1]} /> */}
+    </div>
+  );
 }
 
 export default App;
