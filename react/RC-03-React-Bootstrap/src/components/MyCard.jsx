e8d36098f998caf99b348e2808ab7cbff42a2a4f
KazimAkyol <akyolkzm.2016493@gmail.com>
2024-11-17 17:15:32 +0100

    32.commit

---
+++ react/RC-03-React-Bootstrap/src/components/MyCard.jsx
@@ -0,0 +1,34 @@
+import React from "react";
+import Button from "react-bootstrap/Button";
+import Card from "react-bootstrap/Card";
+import Row from "react-bootstrap/Row";
+import Col from "react-bootstrap/Col";
+
+const MyCard = ({ veri }) => {
+  console.log(veri);
+
+  return (
+    <Row>
+      {veri.map((a) => (
+        <Col
+          lg={4}
+          md={6}
+          sm={12}
+          key={a.id}
+          className="d-flex justify-content-center"
+        >
+          <Card style={{ width: "18rem" }}>
+            <Card.Img variant="top" src={a.img} />
+            <Card.Body>
+              <Card.Title>{a.name}</Card.Title>
+              <Card.Text>{a.text}</Card.Text>
+              <Button variant="primary">Go somewhere</Button>
+            </Card.Body>
+          </Card>
+        </Col>
+      ))}
+    </Row>
+  );
+};
+
+export default MyCard;
