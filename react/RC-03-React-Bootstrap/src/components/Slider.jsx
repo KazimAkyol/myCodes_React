47f724d0a8051213c264a1da7e359dc3c208f2d9
KazimAkyol <akyolkzm.2016493@gmail.com>
2024-11-17 22:15:32 +0100

    37.commit

--- react/RC-03-React-Bootstrap/src/components/Slider.jsx
+++ react/RC-03-React-Bootstrap/src/components/Slider.jsx
@@ -12,21 +12,21 @@ const Slider = () => {
   return (
     <Carousel className="mb-5 carousel-fade">
       <Carousel.Item>
-        <img src={resim1} width="1000px" alt="" />
+        <img src={resim1} width="100%" alt="" />
         <Carousel.Caption>
           <h3>First slide label</h3>
           <p>Nulla vitae elit libero, a pharetra augue mollis interdum.</p>
         </Carousel.Caption>
       </Carousel.Item>
       <Carousel.Item>
-        <img src={resim2} width="1000px" alt="" />
+        <img src={resim2} width="100%" alt="" />
         <Carousel.Caption>
           <h3>Second slide label</h3>
           <p>Lorem ipsum dolor sit amet, consectetur adipiscing elit.</p>
         </Carousel.Caption>
       </Carousel.Item>
       <Carousel.Item>
-        <img src={resim3} width="1000px" alt="" />
+        <img src={resim3} width="100%" alt="" />
         <Carousel.Caption>
           <h3>Third slide label</h3>
           <p>
