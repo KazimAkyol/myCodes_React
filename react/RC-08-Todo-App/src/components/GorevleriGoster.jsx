2b346a412ddb7a0200c03c6bb1967de240265aff
KazimAkyol <akyolkzm.2016493@gmail.com>
2024-11-22 10:10:03 +0100

    52.commit

--- react/RC-08-Todo-App/src/components/GorevleriGoster.jsx
+++ react/RC-08-Todo-App/src/components/GorevleriGoster.jsx
@@ -1,9 +1,29 @@
-import React from 'react'
+import React from "react";
+import { TiDelete } from "react-icons/ti";
 
-const GorevleriGoster = () => {
+const GorevleriGoster = ({ yapilacak, setYapilacak }) => {
   return (
-    <div>GorevleriGoster</div>
-  )
-}
+    <div>
+      {yapilacak.map((a) => (
+        <div
+          key={a.id}
+          className={a.isDone === true ? "done" : "gorev"}
+          onDoubleClick={() =>
+            setYapilacak(
+              yapilacak.map((dizi) =>
+                dizi.id === a.id ? { ...dizi, isDone: !dizi.isDone } : dizi
+              )
+            )
+          }
+        >
+          <h3>
+            {a.text}
+            <TiDelete />
+          </h3>
+        </div>
+      ))}
+    </div>
+  );
+};
 
-export default GorevleriGoster
+export default GorevleriGoster;
