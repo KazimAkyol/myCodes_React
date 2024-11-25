be68a02c306fb073ce6dbd72ce934b3db135f983
KazimAkyol <akyolkzm.2016493@gmail.com>
2024-11-25 23:20:31 +0100

    65.commit

--- react/RC-05-06-Events-Hooks/src/3-form/Form.jsx
+++ react/RC-05-06-Events-Hooks/src/3-form/Form.jsx
@@ -1,5 +1,13 @@
 import React from 'react'
 
+//! JSX ile HTML arasında çok fazla fark olmamakla birlikte bazı önemli farklar da mevcuttur.
+//! HTML'de class olarak tanımlanan nitelik JSX'de className olarak tanımlanmalı.
+//! HTML'de tek kelime olmayan tabindex gibi nitelikler JSX'de camelCase yapısına uygun olarak tabIndex şeklinde dönüştürülür.
+//! HTML'de onclick, onchange gibi nitelikler yukarıdaki camelCase örneğine göre onClick, onChange şeklinde dönüştürülür.
+//! HTML'de label etiketinde belirtilen for attribute'ü JSX'de htmlFor olarak tanımlanmalı.
+//! HTML'de value niteliği JSX'de defaultValue olarak tanımlanmalı.
+//! Ayrıca JSX'de JavaScript ifadeleri {} içinde çalıştırılabilir. Buna map, && gibi yapılar da dahil.
+
 const Form = () => {
   return (
     <div>Form</div>
