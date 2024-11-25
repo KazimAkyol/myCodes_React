1366eee1e8c24ee81ed9ffb6782adf12b409462e
KazimAkyol <akyolkzm.2016493@gmail.com>
2024-11-25 16:17:26 +0100

    59.commit

--- react/RC-09-Appointment/src/components/AddPatient.jsx
+++ react/RC-09-Appointment/src/components/AddPatient.jsx
@@ -1,43 +1,64 @@
-import React, { useState } from 'react'
+import React, { useState } from "react";
+import uuid from "react-uuid";
 
-const AddPatient = ({hastalar, setHastalar}) => {
+const AddPatient = ({ hastalar, setHastalar }) => {
+  const [hastaName, setHastaName] = useState("");
+  const [hastaTarih, setTarih] = useState("");
 
-    const [hastaName, setHastaName] = useState("")
-    const [hastaTarih, setTarih] = useState("")
+  const handleSubmitt = (e) => {
+    e.preventDefault(); // direkt submit olayini yapma, önce alttaki kodlara bak
 
-    const handleSubmitt = (e) => {
-        e.preventDefault() // direkt submit olayini yapma, önce alttaki kodlara bak
+    setHastalar([
+      ...hastalar,
+      {
+        id: uuid(),
+        patientName: hastaName,
+        day: hastaTarih,
+        isDone: false,
+        myDoctor: "DR",
+      },
+    ]);
 
+    // setHastalar'a gidildiginde hemen altta console.log varsa calisiverir ve biz UseState'in yaptigi isi göremeyiz. Bunun icin log bu submit fonksiyonunun disinda olmali.
 
-    }
+    setHastaName("");
+
+    setTarih("");
+    // submit sonrasi inputlardan value temizlemek PictureInPictureWindow, hem alttaki islemler yapilir hem de inputlarda value={isim} yazarak browser'da bosluksa bosluk isimse isim gözükmesi saglanir.
+  };
+
+  console.log(hastalar);
 
   return (
     <div>
-                <form onSubmit={handleSubmitt}>
-          <div className="form-control">
-            <label htmlFor="text">Hasta Bilgileri</label>
-            <input
-              id="text"
-              type="text"
-              onChange={(e)=>setHastaName(e.target.value)}
-            />
-          </div>
-          <div className="form-control">
-            <label htmlFor="day">Day & Time</label>
-            <input
-              id="day"
-              type="datetime-local"
-              onChange={(e)=>setTarih(e.target.value)}
-            />
-          </div>
-          <div>
-            <button type="submit" className="kayit btn-submit">
-              <span style={{color: "#6a0707"}} > dr un adi </span> icin kayit olustur
-            </button>
-          </div>
-        </form>
+      <form onSubmit={handleSubmitt}>
+        <div className="form-control">
+          <label htmlFor="text">Hasta Bilgileri</label>
+          <input
+            id="text"
+            type="text"
+            onChange={(e) => setHastaName(e.target.value)}
+            value={hastaName}
+          />
+        </div>
+        <div className="form-control">
+          <label htmlFor="day">Day & Time</label>
+          <input
+            id="day"
+            type="datetime-local"
+            onChange={(e) => setTarih(e.target.value)}
+            value={hastaTarih}
+          />
+        </div>
+        <div>
+          <button type="submit" className="kayit btn-submit">
+            <span style={{ color: "#6a0707" }}> dr un adi </span> icin kayit
+            olustur
+          </button>
+        </div>
+      </form>
     </div>
-  )
-}
+  );
+};
 
-export default AddPatient
+export default AddPatient;
--- react/RC-09-Appointment/src/helper/Data.jsx
+++ react/RC-09-Appointment/src/helper/Data.jsx
@@ -47,12 +47,14 @@ export const doctorData = [
   {
     id: 1,
     doctorName: "DR Ahmet Bilen",
-    doctorImg: "https://i.cnnturk.com/i/cnnturk/75/0x555/54857220f97adb1aa472e5e6",
+    doctorImg:
+      "https://i.cnnturk.com/i/cnnturk/75/0x555/54857220f97adb1aa472e5e6",
   },
   {
     id: 2,
     doctorName: "DR Ayse Okur",
-    doctorImg: "https://i3.posta.com.tr/i/posta/75/750x0/616f757845d2a0b25401f0e1",
+    doctorImg:
+      "https://i3.posta.com.tr/i/posta/75/750x0/616f757845d2a0b25401f0e1",
   },
   {
     id: 3,
