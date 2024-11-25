48f1e6e018c128e663fd4009236c488457feba60
KazimAkyol <akyolkzm.2016493@gmail.com>
2024-11-25 21:17:16 +0100

    64.commit

--- react/RC-09-Appointment/src/components/PatientList.jsx
+++ react/RC-09-Appointment/src/components/PatientList.jsx
@@ -6,30 +6,36 @@ const PatientList = ({ hastalar, setHastalar, doktorlar }) => {
     <div>
       {hastalar.map((hasta) => (
         <div key={hasta.id}>
-
-{doktorlar.map((a)=>a.doctorName===hasta.myDoctor && (
-    <div
-            className={hasta.isDone ? "trueStil" : "falseStyle"}
-            onDoubleClick={() =>
-              setHastalar(
-                hastalar.map((a) =>
-                  a.id === hasta.id ? { ...a, isDone: !a.isDone } : a
-                )
+          {doktorlar.map(
+            (a) =>
+              a.doctorName === hasta.myDoctor && (
+                <div
+                  className={hasta.isDone ? "trueStil" : "falseStyle"}
+                  onDoubleClick={() =>
+                    setHastalar(
+                      hastalar.map((a) =>
+                        a.id === hasta.id ? { ...a, isDone: !a.isDone } : a
+                      )
+                    )
+                  }
+                >
+                  {/* ilk önce hastalar dizisinde gezip 6 hastayi hastaName'yle bastiriyoruz, sonra onDoubleClick yaptigimiz icin hastalar dizisinde gezinme bitmis oluyor. Dolayisiyla onDoubleClick sonucu gelen setHastalar ile dizide yapacagimiz degisiklikte kimi tikladigimizi belirtmemiz gerekiyor. Bu yüzden tamirci icinde hastalar dizisinde dolasip ekranda tikladigimiz hastamizin id'si ile dizide id'si tutan hastanin diger bilgileri kalsin; isDone'i, varolanin tersiyle degissin. */}
+                  <div>
+                    <h2>{hasta.patientName}</h2>
+                    <h4>{hasta.day}</h4>
+                    <h3>{hasta.myDoctor}</h3>
+                  </div>
+                  <div>
+                    <FaTimesCircle
+                      style={{ color: "red" }}
+                      onClick={() =>
+                        setHastalar(hastalar.filter((a) => a.id !== hasta.id))
+                      }
+                    />
+                  </div>
+                </div>
               )
-            }
-          >
-            <div>
-              <h2>{hasta.patientName}</h2>
-              <h4>{hasta.day}</h4>
-              <h3>{hasta.myDoctor}</h3>
-            </div>
-            <div>
-              <FaTimesCircle style={{ color: "red" }} />
-            </div>
-          </div>
-))}
-
-
+          )}
         </div>
       ))}
     </div>
