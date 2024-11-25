249f9b04d3a542caabc7abfa7769118aa88000bb
KazimAkyol <akyolkzm.2016493@gmail.com>
2024-11-25 14:14:46 +0100

    55.commit

--- react/RC-09-Appointment/src/pages/Home.jsx
+++ react/RC-09-Appointment/src/pages/Home.jsx
@@ -1,9 +1,37 @@
-import React from 'react'
+import React, { useState } from "react";
+
+import { hastaData, doctorData } from "../helper/Data";
 
 const Home = () => {
-  return (
-    <div>Home</div>
-  )
-}
+  const [doktorlar, setDoktorlar] = useState(doctorData);
+  const [hastalar, setPatient] = useState(hastaData);
+  const [show, setShow] = useState(true);
 
-export default Home
+  return (
+    <div>
+      <header className="header">
+        <h1>HOSPITAL</h1>
+
+        <div className="dr">
+            {doktorlar.map((doc)=> (
+                <div>
+                    <img src={doc.doctorImg} alt="" />
+
+                    <h4>
+                    {doc.doctorName}
+                    </h4>
+                </div>
+            ))}
+        </div>
+      </header>
+
+      <div>
+        addpatient
+      </div>
+
+      patientlist
+    </div>
+  );
+};
+
+export default Home;
