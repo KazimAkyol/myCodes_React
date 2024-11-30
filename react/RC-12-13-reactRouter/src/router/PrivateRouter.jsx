daea4de6aa6e7f04aec8cec16fdf747b2360fa9b
KazimAkyol <akyolkzm.2016493@gmail.com>
2024-11-30 23:43:38 +0100

    87.commit

--- react/RC-12-13-reactRouter/src/router/PrivateRouter.jsx
+++ react/RC-12-13-reactRouter/src/router/PrivateRouter.jsx
@@ -1,1 +1,15 @@
+import React from "react";
+import { Navigate, Outlet } from "react-router-dom";
 
+const PrivateRouter = () => {
+  return JSON.parse(localStorage.getItem("email")) === "kazim" &&
+    JSON.parse(localStorage.getItem("pass")) === "6493" ? (
+    <Outlet />
+  ) : (
+    <Navigate to="/login" />
+  );
+};
+
+// kullanici doğru  isim girdiyse Outlet komponenti sayesinde istenilen sayfaya git,yanlışlık varsa Navigate ile bulunduğun yerde (Login) kal ya da başka path veriledebilir.
+
+export default PrivateRouter;
