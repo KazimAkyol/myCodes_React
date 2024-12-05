f0bc321ace6460ab04fecb996d02f93aac08d0ce
KazimAkyol <akyolkzm.2016493@gmail.com>
2024-12-05 15:02:33 +0100

    113.commit

---
+++ react/RC-16-Styled-Component/Styled-Components-1/src/components/Home.jsx
@@ -0,0 +1,28 @@
+import React from 'react'
+import DivStyle from './style/ContainerStyle'
+import HeaderS, { LinkS } from './style/HeaderStyle'
+import ButtonS, { TomatoButton } from './style/ButonStyle'
+
+const Home = () => {
+  return (
+    <div>
+<h1>STYLED COMPONENTS</h1>
+
+<a href="www">LINKS</a>
+
+
+<button >TIKLA1</button>
+<button >TIKLA2</button>
+<button>TIKLA3</button>
+
+
+
+<button>TIKLA4</button>
+<button >TIKLA5</button>
+
+
+    </div>
+  )
+}
+
+export default Home
