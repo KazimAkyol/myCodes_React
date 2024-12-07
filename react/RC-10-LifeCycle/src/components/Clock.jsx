37b3e7796c033eb5a64893922659c97dcc3cd77d
KazimAkyol <akyolkzm.2016493@gmail.com>
2024-12-07 16:18:27 +0100

    127.commit

--- react/RC-10-LifeCycle/src/components/Clock.jsx
+++ react/RC-10-LifeCycle/src/components/Clock.jsx
@@ -8,15 +8,22 @@ import moment from "moment";
 
 const Clock = () => {
   const [zaman, setZaman] = useState(moment);
+  const [count, setCount] = useState(0);
 
   //! Component Did Mount işlemi
   // Yeni Hook useEffect hook'u
 
   useEffect(() => {
-    setInterval(() => {
+    const timer = setInterval(() => {
       console.log("artis var");
       setZaman(moment());
     }, 1000);
+
+    //! ComponentWillUnmount
+    //Componenti öldürdük
+    return () => {
+      setInterval(timer);
+    };
   }, []);
   // Köşeli parantez kullanımı bir defa render ediyor demektir.
 
@@ -27,6 +34,20 @@ const Clock = () => {
           {zaman.format("HH:mm:ss")}
         </h1>
         <p className="text-center text-muted">Anlik Saat</p>
+      </div>
+      <p>**************************</p>
+      <div className="container d-flex justify-content-center mt-3 w-50">
+        <div className="card shadow-lg p-4 rounded">
+          <h1 className="display-4 text-center text-primary">
+            Counter:{count}
+          </h1>
+          <button
+            className="btn btn-success"
+            onClick={() => setCount(count + 1)}
+          >
+            +
+          </button>
+        </div>
       </div>
     </div>
   );
