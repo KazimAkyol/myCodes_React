29a2391a46afec32f8fd87bcf6f79e7746dd2795
KazimAkyol <akyolkzm.2016493@gmail.com>
2024-12-09 09:22:13 +0100

    132.commit

--- react/RC-17-18-RecipeAppProject/src/router/AppRouter.jsx
+++ react/RC-17-18-RecipeAppProject/src/router/AppRouter.jsx
@@ -1,9 +1,11 @@
 import React from "react";
 import { BrowserRouter, Route, Routes } from "react-router-dom";
 import Login from "../pages/login/Login";
-
 import Navbar from "../components/navbar/Navbar";
 import Footer from "../components/footer/Footer";
+import PrivateRouter from "./PrivateRouter";
+import Home from "../pages/home/Home";
+
 const AppRouter = () => {
   return (
     <BrowserRouter>
@@ -12,7 +14,9 @@ const AppRouter = () => {
       <Routes>
         <Route path="/" element={<Login />} />
 
-      
+        <Route path="/home" element={<PrivateRouter />}>
+          <Route path="" element={<Home />} />
+        </Route>
       </Routes>
 
       <Footer />
