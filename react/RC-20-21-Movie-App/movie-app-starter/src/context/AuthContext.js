d4d1592c7f7937c1a17ddd402a37385233407207
KazimAkyol <akyolkzm.2016493@gmail.com>
2024-12-19 21:38:43 +0100

    174.commit

--- react/RC-20-21-Movie-App/movie-app-starter/src/context/AuthContext.js
+++ react/RC-20-21-Movie-App/movie-app-starter/src/context/AuthContext.js
@@ -1,4 +1,6 @@
 import React, { createContext } from "react";
+import { createUserWithEmailAndPassword } from "firebase/auth";
+import { auth } from "../auth/firebase";
 
 //! context alani create edelim:
 export const YetkiContext = createContext();
@@ -7,9 +9,16 @@ const AuthContext = ({ children }) => {
   //! register için (sitede zincir yapılı fetch işlemi var biz burada async await i tercih ettik)
   // https://firebase.google.com/docs/auth/web/start?hl=tr
 
-  const createKullanici=
+  const createKullanici = async (email, password) => {
+    //? sitede ilk defa kullanıcı adı oluşturmak için kullanılan firebase metodu:
+    await createUserWithEmailAndPassword(auth, email, password);
+  };
 
-  return <YetkiContext.Provider>{children}</YetkiContext.Provider>;
+  return (
+    <YetkiContext.Provider value={{ createKullanici }}>
+      {children}
+    </YetkiContext.Provider>
+  );
 };
 
 export default AuthContext;
