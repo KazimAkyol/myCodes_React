2de5e912baa7329d3c2ceca70a3a7108a3bf808f
KazimAkyol <akyolkzm.2016493@gmail.com>
2024-12-20 11:40:59 +0100

    178.commit

--- react/RC-20-21-Movie-App/movie-app-starter/src/context/AuthContext.js
+++ react/RC-20-21-Movie-App/movie-app-starter/src/context/AuthContext.js
@@ -2,11 +2,14 @@ import React, { createContext } from "react";
 import { createUserWithEmailAndPassword } from "firebase/auth";
 import { auth } from "../auth/firebase";
 import { successToast } from "../helpers/ToastNotify";
+import { useNavigate } from "react-router-dom";
 
 //! context alani create edelim:
 export const YetkiContext = createContext();
 
 const AuthContext = ({ children }) => {
+  const navigate = useNavigate();
+  
   //! register için (sitede zincir yapılı fetch işlemi var biz burada async await i tercih ettik)
   // https://firebase.google.com/docs/auth/web/start?hl=tr
 
@@ -15,6 +18,8 @@ const AuthContext = ({ children }) => {
     await createUserWithEmailAndPassword(auth, email, password);
 
     successToast("Kayit basarili");
+
+    navigate("/");
   };
 
   return (
--- react/RC-20-21-Movie-App/movie-app-starter/src/router/AppRouter.jsx
+++ react/RC-20-21-Movie-App/movie-app-starter/src/router/AppRouter.jsx
@@ -3,18 +3,18 @@ import Register from "../pages/Register";
 import Navbar from "../components/Navbar";
 import Main from "../pages/Main";
 import Login from "../pages/Login";
-import { BrowserRouter, Route, Routes } from "react-router-dom";
+import { Route, Routes } from "react-router-dom";
 
 const AppRouter = () => {
   return (
-    <BrowserRouter>
+    <>
       <Navbar />
       <Routes>
         <Route path="/" element={<Main />} />
         <Route path="/login" element={<Login />} />
         <Route path="/register" element={<Register />} />
       </Routes>
-    </BrowserRouter>
+    </>
   );
 };
 
