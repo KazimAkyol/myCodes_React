df687cbcb40d0f9c4e3e94c425cb232b5a89ac86
KazimAkyol <akyolkzm.2016493@gmail.com>
2024-12-26 20:58:17 +0100

    199.commit

--- react/RC-23_redux-intro/src/components/todo/TodoList.js
+++ react/RC-23_redux-intro/src/components/todo/TodoList.js
@@ -1,21 +1,15 @@
-
-
+import { useSelector } from "react-redux";
 import TodoItem from "./TodoItem";
 
 const TodoList = () => {
-    // ekrana basılacak dizi reducers dan gelsin
-
-
-
+  // ekrana basılacak dizi reducer'dan gelsin:
+  const { gorevler } = useSelector((state) => state.todoReducer);
 
   return (
     <div>
-      <div>
-        
-          <TodoItem 
-         />
-    
-      </div>
+      {gorevler.map((gorev) => (
+        <TodoItem key={gorev.id} gorev={gorev} />
+      ))}
     </div>
   );
 };
