2162f13689d399a9d91a7d13463534aa7a82589e
KazimAkyol <akyolkzm.2016493@gmail.com>
2024-12-26 16:34:53 +0100

    197.commit

--- react/RC-23_redux-intro/src/components/counter/Counter.js
+++ react/RC-23_redux-intro/src/components/counter/Counter.js
@@ -1,20 +1,27 @@
-import { useSelector } from "react-redux";
+import { useDispatch, useSelector } from "react-redux";
 import "./Counter.css";
+import { arttir, azalt, sifirla } from "../../redux/actions/counterActions";
 
 const Counter = () => {
   // değişkenler useSelector hookuyla getirilir:
+  const { yazi, sayac } = useSelector((state) => state.counterReducer);
+
+  // fonksiyonları action aracılığıyla reducer'dan getirme komutu:
+  const dispatch1 = useDispatch();
 
   return (
     <div className="app">
       <h2 className="counter-header"> Counter with Redux</h2>
-      <h1></h1>
+      <h1>{sayac}</h1>
       {/* sayac */}
-      <h2></h2>
+
+      <h2>{yazi}</h2>
       {/* yazi */}
       <div>
         <button
           className="counter-button positive"
           // arttır fonksiyonu
+          onClick={() => dispatch1(arttir())}
         >
           ARTTIR
         </button>
@@ -22,12 +29,14 @@ const Counter = () => {
         <button
           className="counter-button zero"
           // sıfırla fonksiyonu
+          onClick={() => dispatch1(sifirla())}
         >
           RESET
         </button>
         <button
           className="counter-button negative"
           // azaltıldı fonksiyonu
+          onClick={() => dispatch1(azalt())}
         >
           AZALT
         </button>
--- react/RC-23_redux-intro/src/redux/reducers/counterReducer.jsx
+++ react/RC-23_redux-intro/src/redux/reducers/counterReducer.jsx
@@ -1,22 +1,16 @@
-import React from 'react'
+import React from "react";
 
-const counterReducer = (state={sayac:0, yazi:""},{type,payload1}) => {
+const counterReducer = (state = { sayac: 0, yazi: "" }, { type, payload1 }) => {
+  switch (type) {
+    case "OKE":
+      return { sayac: state.sayac + 1, yazi: payload1 };
+    case "REDUZIEREN":
+      return { sayac: state.sayac - 1, yazi: payload1 };
+    case "RESETLE":
+      return { sayac: 0 };
+    default:
+      return state;
+  }
+};
 
-
-switch (type) {
-  case "OKE":
-    return{sayac: state.sayac+1,yazi:payload1  }
-   case"REDUZIEREN":
-return
-
-case "RESETLE":
-  return
-  default:
-    return state;
-}
-
-
-
-}
-
-export default counterReducer
+export default counterReducer;
