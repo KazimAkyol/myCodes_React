84e272744191420d8af074ae0e075b6664af78fc
KazimAkyol <akyolkzm.2016493@gmail.com>
2025-01-02 14:21:10 +0100

    209.commit

--- react/RC-24-redux-toolkit-example/src/pages/Login.jsx
+++ react/RC-24-redux-toolkit-example/src/pages/Login.jsx
@@ -1,9 +1,51 @@
-import React from 'react'
+import {
+  Avatar,
+  Box,
+  Button,
+  Container,
+  TextField,
+  Typography,
+} from "@mui/material";
+import React from "react";
 
 const Login = () => {
   return (
-    <div>Login</div>
-  )
-}
+    <Container component="main" maxWidth="xs">
+      <Box
+        sx={{
+          marginTop: 8,
+          display: "flex",
+          flexDirection: "column",
+          alignItems: "center",
+        }}
+      >
+        <Avatar
+          sx={{ width: 100, height: 100 }}
+          src="https://cdn.pixabay.com/photo/2017/03/21/02/00/user-2160923_960_720.png"
+        />
+        <Typography component="h1" variant="h5">
+          Sign In
+        </Typography>
+        {/* noValidate=doğrulanmasın
+        autocomplete=Otomatik Tamamlama listesi, siz yazmaya başladığınızda adlara ve e-posta adreslerine ilişkin önerileri görüntüleyen bir özelliktir. Bu öneriler, gönderdiğiniz e-posta mesajlarındaki ad ve e-posta adresleri listesindeki olası eşleşmelerdir.
+         */}
+        <Box component="form" sx={{ mt: 1 }}>
+          <TextField
+            margin="normal"
+            required
+            fullWidth
+            id="email"
+            label="Email Address"
+            name="email"
+            autoComplete="email"
+            autoFocus
+          />
+          <TextField />
+          <Button>Sign In</Button>
+        </Box>
+      </Box>
+    </Container>
+  );
+};
 
-export default Login
+export default Login;
