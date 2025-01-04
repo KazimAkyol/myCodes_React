b255b0ce36944bcf1ba1d2fe650c1f601bec887c
KazimAkyol <akyolkzm.2016493@gmail.com>
2025-01-04 16:29:42 +0100

    212.commit

--- react/RC-24-redux-toolkit-example/src/App.js
+++ react/RC-24-redux-toolkit-example/src/App.js
@@ -1,14 +1,13 @@
-import AppRouter from './router/AppRouter';
-
+import { store } from "./app/store";
+import AppRouter from "./router/AppRouter";
+import Provider from "react-router-dom";
 
 function App() {
-
-  
   return (
     <div>
-
+      <Provider store={store}>
         <AppRouter />
-    
+      </Provider>
     </div>
   );
 }
--- react/RC-24-redux-toolkit-example/src/app/store.jsx
+++ react/RC-24-redux-toolkit-example/src/app/store.jsx
@@ -1,5 +1,10 @@
+import { configureStore } from "@reduxjs/toolkit";
+import haberSlice from "../features/haberSlice";
+import yetkiSlice from "../features/yetkiSlice";
+
 export const store = configureStore({
   reducer: {
-    counter: counterReducer,
+    yetkiSlice: yetkiSlice,
+    haberSlice: haberSlice,
   },
 });
