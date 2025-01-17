e1b97c986624c5075f45971b89c1e7834d9f595a
KazimAkyol <akyolkzm.2016493@gmail.com>
2025-01-17 23:46:53 +0100

    243.commit

--- react/RC-24-30-Stock-App/inclass-formik-yup/src/pages/Firms.jsx
+++ react/RC-24-30-Stock-App/inclass-formik-yup/src/pages/Firms.jsx
@@ -1,18 +1,59 @@
 import React, { useEffect } from "react";
 import useStockCall from "../hook/useStockCall";
 import { useSelector } from "react-redux";
+import { Button } from "@mui/material";
+import Typography from "@mui/material/Typography";
+import Grid from "@mui/material/Grid";
 
 const Firms = () => {
   const { getStockData } = useStockCall();
 
   const { firms } = useSelector((state) => state.stock);
-  console.log(firms);
 
   useEffect(() => {
     getStockData("firms");
   }, []);
 
-  return <div>Firms</div>;
+  return (
+    <div>
+      <Typography variant="h4" sx={{ color: "red", marginBottom: "1rem" }}>
+        Firms
+      </Typography>
+
+      <Button
+        onClick={handleOpen}
+        sx={{
+          backgroundColor: "secondary.main",
+          color: "white",
+          padding: "0.2rem 1rem",
+          marginBottom: "1rem",
+          "&:hover": {
+            backgroundColor: "secondary.main",
+          },
+        }}
+      >
+        New Firm
+      </Button>
+      {open && (
+        <FirmModal
+          open={open}
+          handleClose={handleClose}
+          initialState={initialState}
+        />
+      )}
+      <Grid container sx={{ marginLeft: "1rem" }}>
+        {firms.map((firm, index) => (
+          <Grid key={index} size={{ xs: 2, sm: 4, md: 4 }}>
+            <FirmCard
+              {...firm}
+              handleOpen={handleOpen}
+              setInitialState={setInitialState}
+            />
+          </Grid>
+        ))}
+      </Grid>
+    </div>
+  );
 };
 
 export default Firms;
