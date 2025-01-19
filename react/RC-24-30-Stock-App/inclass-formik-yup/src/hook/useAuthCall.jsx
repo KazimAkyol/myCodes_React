446bc886b6409f8471057ea17d8ad8fa49d634e6
KazimAkyol <akyolkzm.2016493@gmail.com>
2025-01-19 22:16:09 +0100

    248.commit

--- react/RC-24-30-Stock-App/inclass-formik-yup/src/hook/useAuthCall.jsx
+++ react/RC-24-30-Stock-App/inclass-formik-yup/src/hook/useAuthCall.jsx
@@ -1,6 +1,5 @@
 import React from "react";
-import axios from "axios";
-import { useDispatch, useSelector } from "react-redux";
+import { useDispatch } from "react-redux";
 import {
   fetchFail,
   fetchStart,
@@ -10,27 +9,30 @@ import {
 } from "../features/authSlice";
 import { useNavigate } from "react-router-dom";
 import useAxios from "./useAxios";
+import { toastErrorNotify, toastSuccessNotify } from "../helper/ToastNotify";
 
 const useAuthCall = () => {
   const dispatch = useDispatch();
   const navigate = useNavigate();
-  const { axiosWithToken } = useAxios();
+  const { axiosWithToken, axiosWithoutHeader } = useAxios();
 
-  //! Custom hook yazma kuralları:
-  //? 1- use kelimesi ile başlar
-  //? 2- return de { fonksiyonlar }, değişkense [ bilgiler ] gönderilmeli
-  //? 3- Çağrılacağı noktada const {register}=useAuthCall() seklinde tanimlanir.
+  // Custom hook yazma kuralları
+  //? 1-use Kelimesi ile başlar
+  //? 2-return'de { fonksiyonlar }, değişkense [ bilgiler ] gönderilmeli
+  //? 3-cağrılacağı noktada
+  //? const {register}=useAuthCall()
 
   const register = async (userInfo) => {
     dispatch(fetchStart());
 
     try {
-      const { data } = await axiosWithToken.get(`${BASE_URL}users`, userInfo);
-      console.log("register icinde", data);
+      const { data } = await axiosWithoutHeader.post(`users`, userInfo);
       dispatch(registerSuccess(data));
       navigate("/stock");
+      toastSuccessNotify("Register is successful");
     } catch (error) {
       dispatch(fetchFail());
+      toastErrorNotify("Register failed");
     }
   };
 
@@ -38,12 +40,13 @@ const useAuthCall = () => {
     dispatch(fetchStart());
 
     try {
-      const { data } = await axios.post(`${BASE_URL}auth/login`, userInfo);
-      console.log("register icinde", data);
+      const { data } = await axiosWithoutHeader.post(`auth/login`, userInfo);
       dispatch(loginSuccess(data));
       navigate("/stock");
+      toastSuccessNotify("Login is successful");
     } catch (error) {
       dispatch(fetchFail());
+      toastErrorNotify("Login failed");
     }
   };
 
@@ -51,12 +54,10 @@ const useAuthCall = () => {
     dispatch(fetchStart());
 
     try {
-      const { data } = await axios(`${BASE_URL}auth/logout`, {
-        headers: {
-          Authorization: `Token ${token}`,
-        },
-      });
+      const { data } = await axiosWithToken.get(`auth/logout`);
       dispatch(logoutSuccess());
+      toastSuccessNotify("Logout is successful");
+
       navigate("/");
     } catch (error) {
       dispatch(fetchFail());
