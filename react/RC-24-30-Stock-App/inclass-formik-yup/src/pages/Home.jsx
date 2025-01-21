989081bf36e4be729b6f022e61ace7930d1093ba
KazimAkyol <akyolkzm.2016493@gmail.com>
2025-01-21 16:03:26 +0100

    261.commit

--- react/RC-24-30-Stock-App/inclass-formik-yup/src/pages/Home.jsx
+++ react/RC-24-30-Stock-App/inclass-formik-yup/src/pages/Home.jsx
@@ -1,9 +1,14 @@
-import React from 'react'
+import React from "react";
+import KpiCard from "../components/KpiCard";
+import Charts from "../components/Charts";
 
 const Home = () => {
   return (
-    <div>Home</div>
-  )
-}
+    <div>
+      <KpiCard />
+      <Charts />
+    </div>
+  );
+};
 
-export default Home
+export default Home;
