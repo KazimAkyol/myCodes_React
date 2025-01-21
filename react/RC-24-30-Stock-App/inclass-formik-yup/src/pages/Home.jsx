cd453b8a82ad08ecd251c1de4beaed92fe2c7b1c
KazimAkyol <akyolkzm.2016493@gmail.com>
2025-01-21 16:23:56 +0100

    262.commit

--- react/RC-24-30-Stock-App/inclass-formik-yup/src/pages/Home.jsx
+++ react/RC-24-30-Stock-App/inclass-formik-yup/src/pages/Home.jsx
@@ -1,8 +1,15 @@
 import React from "react";
-import KpiCard from "../components/KpiCard";
-import Charts from "../components/Charts";
+import KpiCard from "./../components/KpiCard";
+import Charts from "./../components/Charts";
+import { useEffect } from "react";
+import useStockCall from "../hook/useStockCall";
 
 const Home = () => {
+  const { getPurcSales } = useStockCall();
+  useEffect(() => {
+    getPurcSales();
+  }, []);
+
   return (
     <div>
       <KpiCard />
